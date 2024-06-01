138a3eb60a8e72e0703efbce5d15b1ea9157415d
Solype <ambroise.jacquemet-ramirez-vega@epitech.eu>
2024-06-01 11:15:26 +0200

    [FIX] main feature

--- back/index.js
+++ back/index.js
@@ -8,38 +8,34 @@ dotenv.config();
 const app = express();
 const port = 5000;
 
-const OpenAIService = {
-    getGPT3Response: async (userPrompt) => {
-        const systemInstructions = "";
-        const prompt = `${systemInstructions}\n${userPrompt}`;
+const OpenAI = require('openai');
+const openai = new OpenAI();
 
-        const response = await axios.post('https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions', {
-            prompt: prompt,
-            max_tokens: 100,
-        }, {
-            headers: {
-                'Authorization': `Bearer ${process.env.OPENAI_PROJECT_API_KEY}`
-            }
-        });
-        return response.data.choices[0].text;
-    }
-};
+let discussions = {};
 
 app.use(cors())
 app.use(bodyParser.json());
 
-app.post("/api/create", (req, res) => {
-    
-});
 
 app.get("/api/data", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
 app.post("/api/data", async (req, res) => {
+    const message = req.body.message;
+    const title = req.body.title;
+
+    console.log(discussions[title]);
+    if (discussions[title] === undefined) {
+        discussions[title] = [{"role": "system", "content": "Theo, an IA that teachs really well programmation language"}];
+    }
+
+    discussions[title].push({"role": "user", "content": message});
+
     try {
-        const responseText = await OpenAIService.getGPT3Response(req.body.message);
-        res.json({ text: responseText });
+        const responseText = await openai.chat.completions.create({ messages: discussions[title], model: "gpt-4o"});
+        discussions[title].push({"role": "assistant", "content": responseText.choices[0].message.content});
+        res.json({ text: responseText.choices[0].message.content });
     } catch (error) {
         console.error(error.response ? error.response.data : error.message);
         res.status(500).send('Une erreur est survenue');
