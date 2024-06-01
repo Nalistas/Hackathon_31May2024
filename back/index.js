87e26390c3d067dffa39558223d8413a79d609a4
htesson <hugo.tesson@epitech.eu>
2024-06-01 09:18:05 +0200

    [ADD] chatgpt intergration

--- back/index.js
+++ back/index.js
@@ -3,12 +3,25 @@ const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const app = express();
 const port = 3000;
 
-// Charger la clé API de projet à partir du fichier .env
-const API_KEY = process.env.OPENAI_PROJECT_API_KEY;
+const OpenAIService = {
+    getGPT3Response: async (userPrompt) => {
+        const systemInstructions = "";
+        const prompt = `${systemInstructions}\n${userPrompt}`;
+
+        const response = await axios.post('https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions', {
+            prompt: prompt,
+            max_tokens: 100,
+        }, {
+            headers: {
+                'Authorization': `Bearer ${process.env.OPENAI_PROJECT_API_KEY}`
+            }
+        });
+        return response.data.choices[0].text;
+    }
+};
 
 app.use(express.json());
 
@@ -16,17 +29,8 @@ app.post('/api/generate', async (req, res) => {
     const { prompt } = req.body;
 
     try {
-        const response = await axios.post('https://api.openai.com/v1/engines/Theo/completions', {
-            prompt: prompt,
-            max_tokens: 150,
-        }, {
-            headers: {
-                'Authorization': `Bearer ${API_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        });
-
-        res.json(response.data);
+        const responseText = await OpenAIService.getGPT3Response(prompt);
+        res.json({ text: responseText });
     } catch (error) {
         console.error(error.response ? error.response.data : error.message);
         res.status(500).send('Une erreur est survenue');
